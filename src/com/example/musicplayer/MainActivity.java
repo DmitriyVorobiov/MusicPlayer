1cf8e0e522cae15cbcc860b534b56d0d17c38550
DmitriyVorobiov <vorobiov17@yandex.ru>
2014-10-22 00:06:31 +0400

    написал функционал сервиса, связь не доделана

--- src/com/example/musicplayer/MainActivity.java
+++ src/com/example/musicplayer/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.musicplayer;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 
 public class MainActivity extends Activity {
+
+	private BroadcastReceiver broadcastReceiver;
+	private ServiceConnection serviceConnection;
+	private boolean connectedWithService;
+	private MusicPlayerService musicService;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// TODO init
+
+		startService(new Intent(this, MusicPlayerService.class));
+		serviceConnection = new ServiceConnection() {
+			@Override
+			public void onServiceDisconnected(ComponentName name) {
+				connectedWithService = false;
+				musicService = null;
+			}
+
+			@Override
+			public void onServiceConnected(ComponentName name,
+					IBinder musicBinder) {
+				musicService = ((MusicPlayerService.MusicBinder) musicBinder)
+						.getMusicPlayerService();
+				connectedWithService = true;
+				updateUI();
+			}
+		};
+
+		broadcastReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				updateUI();
+			}
+		};
+		LocalBroadcastManager.getInstance(this).registerReceiver(
+				broadcastReceiver, new IntentFilter(ACTION_PLAYER));
+
+	}
+
+	protected void updateUI() {
+		int musicStatus = 0;
+		if (connectedWithService) {
+			musicStatus = musicService.getPlayingStatus();
+		} else {
+			musicStatus = MusicPlayerService.STATUS_IDLE;
+		}
+		switch (musicStatus) {
+		case MusicPlayerService.STATUS_IDLE:
+
+			break;
+		case MusicPlayerService.STATUS_PLAYING:
+
+			break;
+		case MusicPlayerService.STATUS_PAUSED:
+
+			break;
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		bindService(new Intent(this, MusicPlayerService.class),
+				serviceConnection, 0);
+		if (connectedWithService) {
+			//musicService.onRecieveCommand();
+		}
 	}
 }
--- src/com/example/musicplayer/MusicPlayerService.java
+++ src/com/example/musicplayer/MusicPlayerService.java
@@ -1,19 +1,100 @@
 package com.example.musicplayer;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 
-public class MusicPlayerService extends Service {
+public class MusicPlayerService extends Service implements OnCompletionListener {
 
-	public MusicPlayerService() {
-		// TODO Auto-generated constructor stub
+	private static MediaPlayer mediaPlayer;
+	private MusicBinder binder;
+	private int playingStatus;
+	protected final static int STATUS_IDLE = 0;
+	protected final static int STATUS_PLAYING = 1;
+	protected final static int STATUS_PAUSED = 2;
+
+	class MusicBinder extends Binder {
+		MusicPlayerService getMusicPlayerService() {
+			return MusicPlayerService.this;
+		}
 	}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return binder;
 	}
 
+	public void onCompletion(MediaPlayer arg0) {
+		playingStatus = STATUS_IDLE;
+		sendResult();
+		releaseMediaPlayer();
+		stopForeground(true);
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		binder = new MusicBinder();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		releaseMediaPlayer();
+		mediaPlayer = null;
+	}
+
+	private void releaseMediaPlayer() {
+		if (mediaPlayer != null) {
+			if (mediaPlayer.isPlaying())
+				mediaPlayer.stop();
+			mediaPlayer.release();
+			mediaPlayer = null;
+		}
+	}
+
+	void onRecieveCommand() {
+		switch (playingStatus) {
+		case STATUS_IDLE:
+			releaseMediaPlayer();
+			// mediaPlayer = new MediaPlayer();
+			mediaPlayer = MediaPlayer.create(this, R.raw.audio);
+			// mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+			// mediaPlayer.setOnCompletionListener(this);
+			// mediaPlayer.prepare();
+			mediaPlayer.start();
+			playingStatus = STATUS_PLAYING;
+
+			break;
+		case STATUS_PLAYING:
+			mediaPlayer.pause();
+			playingStatus = STATUS_PAUSED;
+
+			break;
+		case STATUS_PAUSED:
+			mediaPlayer.start();
+			playingStatus = STATUS_PLAYING;
+			break;
+		}
+		sendResult();
+		if (playingStatus == STATUS_PLAYING) {
+			startForeground(1, new Notification());
+		} else {
+			stopForeground(true);
+		}
+	}
+
+	private void sendResult() {
+		Intent intent = new Intent(MainActivity.ACTION_PLAYER);
+		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+	}
+
+	int getPlayingStatus() {
+		return playingStatus;
+	}
 }
