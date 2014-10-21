d8ad60e83916b7f97ddfb18421ea835b12453e53
DmitriyVorobiov <vorobiov17@yandex.ru>
2014-10-21 22:09:05 +0400

    test commit, application created, service created

---
+++ src/com/example/musicplayer/MusicPlayerService.java
@@ -0,0 +1,19 @@
+package com.example.musicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MusicPlayerService extends Service {
+
+	public MusicPlayerService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
